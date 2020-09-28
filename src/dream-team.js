fcbc6c381414abf4bb7925fb75d48411e65ee562
Madina <madina.karabalina@gmail.com>
2020-09-28 14:39:36 +0600

    feat: finish dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
+module.exports = function createDreamTeam(members) {
   // remove line with error and write your code here
+  let resultString = '';
+  let bool = false;
+  let arr = [];
+  if (members === undefined || members === null) {
+    return bool;
+  }
+
+  for (let i = 0; i < members.length; i+=1) {
+    if (typeof members[i] === 'string') {
+      bool = true;
+      arr[i] = members[i].trim();
+      resultString += arr[i][0].toUpperCase(); 
+    }
+  }
+  if (bool = false) {
+    return bool;
+  }
+
+  return resultString.split('').sort().join('');
 };
