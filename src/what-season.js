05c47257cdf67ce022882676b4962b370c07d431
Madina <madina.karabalina@gmail.com>
2020-10-16 15:39:22 +0600

    feat: finish season

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
+module.exports = function getSeason(date) {
+  // throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  let months = {1: 'winter',
+                2: 'winter',
+                3: 'spring',
+                4: 'spring',
+                5: 'spring',
+                6: 'summer',
+                7: 'summer',
+                8: 'summer',
+                9: 'autumn',
+                10: 'autumn',
+                11: 'autumn',
+                12: 'winter'};
+
+  if (date === undefined) {
+    return 'Unable to determine the time of year!'
+  }
+  if (isNaN(date)) {
+    throw 'Error';
+  }
+
+  let dateL = date.toLocaleDateString();
+  let month = '';
+  for (let i = 0; i < dateL.length; i+=1) {
+    if (dateL[i] === '/') {
+      break;
+    }
+    month += dateL[i];
+  }
+  // month = parseInt(month);
+  for (let key in months) {
+    if (key === month) {
+      return months[key];
+    }
+  }
 };
