1d8ba189a025eb5d6dfca845b34d23a478ecc706
Madina <madina.karabalina@gmail.com>
2020-10-17 12:13:58 +0600

    feat: add carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,16 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/*sampleActivity*/) {
-  throw new CustomError('Not implemented');
+module.exports = function dateSample(sampleActivity) {
+  // throw new CustomError('Not implemented');
   // remove line with error and write your code here
-};
+  if (typeof(sampleActivity) === 'string') {
+    sampleActivity = parseFloat(sampleActivity);
+    if (sampleActivity <= 0 || sampleActivity > MODERN_ACTIVITY || !isFinite(sampleActivity)) {
+      return false;
+    } 
+    return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD));
+  } else {
+    return false;
+  }
+}
